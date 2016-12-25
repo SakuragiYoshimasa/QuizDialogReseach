4d0dbfd40fd98c53492fd1e4a2e8d3567b3c0ebf
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2016-12-25 18:13:51 +0900

    Made animation controller

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : Singleton<AnimationController> {
+
+	[SerializeField]
+	private Animator animator;
+
+}
